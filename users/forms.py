33104be2307359d569a546fa94a9f3cc9e6f441c
Kkakashiii513 <kkakashi513hatakeeee@gmail.com>
2025-06-16 08:17:10 +0500

    registration is ready

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,36 @@
 from dataclasses import field
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.db.models import Model
 
 from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
+
+    username = forms.CharField()
+    password = forms.CharField()
+
     class Meta:
         model = User
         fields = ["username", "password"]
+
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+        )
--- users/views.py
+++ users/views.py
@@ -1,11 +1,11 @@
 from pdb import post_mortem
 from django.contrib import auth
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse
 
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def profile(request):
@@ -18,8 +18,21 @@ def profile(request):
 
 def registration(request):
 
+    if request.method == "POST":
+
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse("main:index"))
+
+    else:
+        form = UserRegistrationForm()
+
     context = {
         "title": "Home - registration",
+        "form": form,
     }
 
     return render(request, "users/registration.html", context)
@@ -30,7 +43,7 @@ def login(request):
     if request.method == "POST":
 
         form = UserLoginForm(data=request.POST)
-        if form.is_valid:
+        if form.is_valid():
             username = request.POST["username"]
             password = request.POST["password"]
             user = auth.authenticate(username=username, password=password)
@@ -51,4 +64,6 @@ def login(request):
 
 
 def logout(request):
-    pass
+    auth.logout(request)
+
+    return redirect(reverse("main:index"))
