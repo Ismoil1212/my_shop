c8041540018c5a634df1d2e89d71b8b6049e8bce
Kkakashiii513 <kkakashi513hatakeeee@gmail.com>
2025-07-18 17:11:00 +0500

    добавил whitenoise

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -97,6 +98,7 @@ DATABASES = {
     "default": env.db(default="postgres://shop_admin:zxc^ ,^@localhost:1206/shop")
 }
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 CACHES = {
     "default": {
