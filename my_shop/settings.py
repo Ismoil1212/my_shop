231f001b7a45412a0ad155d64d3faebc4919e9ec
Kkakashiii513 <kkakashi513hatakeeee@gmail.com>
2025-07-21 12:32:58 +0500

    маиграции

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -34,12 +34,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
-
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+
 RENDER_HOSTNAME = env("RENDER_EXTERNAL_HOSTNAME", default=None)
 if RENDER_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_HOSTNAME)
